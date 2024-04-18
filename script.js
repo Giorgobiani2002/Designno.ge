7f71e537dd65b97e34a59e608100b3129b2f7a09
Giorgobiani2002 <160760301+Giorgobiani2002@users.noreply.github.com>
2024-04-18 20:23:42 +0400

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+function showSideBar() {
+  const sideBar = document.querySelector(".navbar-menu2");
+  sideBar.style.display = "flex";
+}
+function closeSideBar() {
+    const closeBar = document.querySelector(".navbar-menu2");
+    closeBar.style.display="none";
+}
+
+
